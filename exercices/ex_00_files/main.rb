5018b79a2fbb210a1d085f80d3f3b6e53e7ff408
Benjamin Delacombaz <benjamin.delacombaz@cpnv.ch>
2018-05-04 08:27:47 +0200

    Finish ex_00_files

--- exercices/ex_00_files/main.rb
+++ exercices/ex_00_files/main.rb
@@ -1,13 +1,13 @@
-def findAllFilePathStartingWith(starts, path)
+def findAllFilePathStartingWith(starts, path, list = Array.new)
   Dir.children(path).each { |child| 
     if File.directory?(File.join(path, child))
-      findAllFilePathStartingWith(starts, File.join(path, child))
+      findAllFilePathStartingWith(starts, File.join(path, child), list)
     else
       if child.start_with? starts
-        puts File.join(path, child)
+        list.push(File.join(path, child))
       end
     end
   }
+  list
 end
-
-findAllFilePathStartingWith('read', './arbo/')
+puts findAllFilePathStartingWith('read', './arbo/')
