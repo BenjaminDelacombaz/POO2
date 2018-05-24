cbcebfc4c261e4100720a5d975da0b11bb47dfea
Benjamin Delacombaz <benjamin.delacombaz@cpnv.ch>
2018-05-24 13:33:06 +0200

    Continue exercice word search tree

--- exercices/ex_03_arbre/main.rb
+++ exercices/ex_03_arbre/main.rb
@@ -1,2 +1,11 @@
 require_relative('word_search_tree')
 
+file = File.open('functions.txt', 'rb')
+tree = WordSearchTree.new
+file.read.gsub(/\s+/m, ' ').gsub(/^\s+|\s+$/m, '').split(" ").each do |word|
+ tree.add_word(word)
+end
+
+puts tree.root.childs
+
+# puts tree.search('x'.ord)
--- exercices/ex_03_arbre/word_search_tree.rb
+++ exercices/ex_03_arbre/word_search_tree.rb
@@ -1,17 +1,33 @@
 class WordSearchTree
+  attr_accessor :root
   class Node
-    def initialize(data, parent)
+    attr_accessor :childs
+    def initialize(data)
+      @is_word = false
       @data = data
-      @parent = parent
-      @childs = Array.new
+      @childs = Hash.new
     end
   end
 
   def initialize()
-    @root = Node.new(nil,nil)
+    @root = Node.new(nil)
   end
 
-  def add_child(data)
-    
+  def search(letter)
+    @root.childs[letter]
+  end
+
+  def add_word(word)
+    add_child(@root, word)
+  end
+
+  private def add_child(current, rest)
+    letter = rest.chars.shift
+    if !current.childs.has_key?(letter.ord)
+      current.childs[letter.ord] = Node.new(letter.ord)
+    end
+    if !rest.empty?
+      add_child(current.childs[letter.ord], rest)
+    end
   end
 end
