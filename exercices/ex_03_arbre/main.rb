cb53222455b8a9ed37c0b61935675dcbb61c8d79
Benjamin Delacombaz <benjamin.delacombaz@cpnv.ch>
2018-05-25 08:39:13 +0200

    Finish part 3 ex 03

--- exercices/ex_03_arbre/main.rb
+++ exercices/ex_03_arbre/main.rb
@@ -6,6 +6,11 @@ file.read.gsub(/\s+/m, ' ').gsub(/^\s+|\s+$/m, '').split(" ").each do |word|
  tree.add_word(word)
 end
 
-puts tree.root.childs
+# puts tree.root.childs
 
-# puts tree.search('x'.ord)
+# puts tree.search('z'.ord).childs
+
+puts 'Veuillez choisir un nom de fonctions.'
+search = gets
+
+puts tree.exists?(search)
--- exercices/ex_03_arbre/word_search_tree.rb
+++ exercices/ex_03_arbre/word_search_tree.rb
@@ -1,7 +1,7 @@
 class WordSearchTree
   attr_accessor :root
   class Node
-    attr_accessor :childs
+    attr_accessor :childs, :is_word
     def initialize(data)
       @is_word = false
       @data = data
@@ -13,6 +13,18 @@ class WordSearchTree
     @root = Node.new(nil)
   end
 
+  def exists?(rest, current = @root)
+    letterList = rest.chars
+    letter = letterList.shift
+    rest = letterList.join
+
+    if !rest.empty? && current.childs.has_key?(letter.ord)
+      exists?(rest, current.childs[letter.ord])
+    else
+      current.is_word
+    end
+  end
+  
   def search(letter)
     @root.childs[letter]
   end
@@ -22,12 +34,16 @@ class WordSearchTree
   end
 
   private def add_child(current, rest)
-    letter = rest.chars.shift
+    letterList = rest.chars
+    letter = letterList.shift
+    rest = letterList.join
     if !current.childs.has_key?(letter.ord)
-      current.childs[letter.ord] = Node.new(letter.ord)
+      current.childs[letter.ord] = Node.new(letter)
     end
     if !rest.empty?
       add_child(current.childs[letter.ord], rest)
+    else
+      current.childs[letter.ord].is_word = true
     end
   end
 end
