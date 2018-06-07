05d8a14ae3f9739cc5bdc11d15989dd1e08c32dd
Benjamin Delacombaz <benjamin.delacombaz@cpnv.ch>
2018-06-07 08:55:50 +0200

    Add test not valid

--- exercices/ex_06_chain_of_responsability/main.rb
+++ exercices/ex_06_chain_of_responsability/main.rb
@@ -5,3 +5,8 @@ inputs = [Input.new('Tutu', 'text', { "max_lenght" => 10, "min_lenght" => 3 }), 
 form = UserForm.new(inputs)
 
 puts "Le formulaire est-il valide ? #{form.isValid?}"
+
+inputs = [Input.new('Tutu', 'number', { "max_lenght" => 10, "min_lenght" => 3 }), Input.new('ben@gmail.com', 'email', {}), Input.new(19, 'number', {})]
+form = UserForm.new(inputs)
+
+puts "Le formulaire est-il valide ? #{form.isValid?}"
